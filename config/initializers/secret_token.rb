fec1c4fbe6ba41a71d4ebf458816e516d515f047
Henry Happ <henry.happ@comprometa.org>
2010-09-22 20:42:36 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '35cfdfdb2d8956b4f492e9ab0b3184957b5dd681d64926cde4ad1df620dd23c13423e211dc194fcbc2ead85c5c1d967a739e9c9e0eb34a9b5c15d258d71ab7e5'
